2c3ce5ebaaef3b22a8a2260db7b0c5c77c172d5c
Andrii Baidachenko <andrewbayd@gmail.com>
2018-12-01 17:40:08 +0200

    Started implementation of SearchPage class

--- src/main/java/Pages/SearchPage.java
+++ src/main/java/Pages/SearchPage.java
@@ -1,4 +1,22 @@
 package Pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class SearchPage {
+    private static final String FILTER_YEAR_XPATH = "//div[@data-qa-selector='filter-year']";
+    private static final String YEAR_RANGE_DROPDOWN_XPATH = "//select[@name='yearRange.min']";
+    private static final String YEAR_RANGE_OPTIONS_XPATH = "//select[@name='yearRange.min']/option";
+
+    @FindBy(xpath = FILTER_YEAR_XPATH)
+    private WebElement filterYear;
+
+    @FindBy(xpath = YEAR_RANGE_DROPDOWN_XPATH)
+    private WebElement yearRangeDropdown;
+
+    @FindBy(xpath = YEAR_RANGE_OPTIONS_XPATH)
+    private List<WebElement> yearRangeOptions;
+    
 }
