17b9d6fc6d7af0ad3ad0534bbce72e0270f5514b
Andrii Baidachenko <andrewbayd@gmail.com>
2018-12-01 17:08:59 +0200

    Implemented Navigator class

--- src/main/java/Utils/Navigator.java
+++ src/main/java/Utils/Navigator.java
@@ -1,4 +1,12 @@
 package Utils;
 
+import static Utils.DriverWrapper.driver;
+
 public class Navigator {
+    private static final String HOMEPAGE_URL = "https://www.autohero.com/de/";
+
+    public void openHomePage() {
+        driver.get(HOMEPAGE_URL);
+        System.out.println("Home page was opened");
+    }
 }
